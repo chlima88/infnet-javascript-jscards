30de91809bfda27e40d75fe5f63f400d5dcf9a42
Charles Costa <chlima88@gmail.com>
2022-05-31 02:17:49 -0300

    add player rating data, back card and transitin effects

--- data.js
+++ data.js
@@ -3,60 +3,530 @@ export let data = [
     "logo":"https://img-cdn.hltv.org/teamlogo/WF5P55R5fxcmhoew6vHZQr.png?ixlib=java-2.1.0&w=100&s=69a1bfb54d6350db4a0b18ff87a96dcc",
     "name": "Imperial",
     "points": 150,
-    "rank": 10
+    "rank": 10,
+    "roster": [
+      {
+        "player": "fnx",
+        "stats": {
+          "rating": 0.94,
+          "kpr": 0.56,
+          "headshots": 0.48,
+          "dpr": 0.61
+        }
+      },
+      {
+        "player": "FalleN",
+        "stats": {
+          "rating": 1.11,
+          "kpr": 0.69,
+          "headshots": 0.303,
+          "dpr": 0.59
+        }
+      },
+      {
+        "player": "fer",
+        "stats": {
+          "rating": 1.22,
+          "kpr": 0.79,
+          "headshots": 0.428,
+          "dpr": 0.64
+        }
+      },
+      {
+        "player": "boltz",
+        "stats": {
+          "rating": 1.12,
+          "kpr": 0.67,
+          "headshots": 0.354,
+          "dpr": 0.60
+        }
+      },
+      {
+        "player": "VINI",
+        "stats": {
+          "rating": 1.1,
+          "kpr": 0.68,
+          "headshots": 0.419,
+          "dpr": 0.63
+        }
+      }
+    ]
   },
   {
     "logo": "https://img-cdn.hltv.org/teamlogo/bEgST6XoNV4ZdenRKzCQyl.svg?ixlib=java-2.1.0&s=bd9b10a8dfe7b3640103745687389e3c",
     "name": "Cloud9",
     "points": 293,
-    "rank": 9
+    "rank": 9,
+    "roster": [
+      {
+        "player": "HObbit",
+        "stats": {
+          "rating": 1.17,
+          "kpr": 0.74,
+          "headshots": 0.46,
+          "dpr": 0.64
+        }
+      },
+      {
+        "player": "interz",
+        "stats": {
+          "rating": 1.0,
+          "kpr": 0.58,
+          "headshots": 0.483,
+          "dpr": 0.55
+        }
+      },
+      {
+        "player": "Ax1le",
+        "stats": {
+          "rating": 1.22,
+          "kpr": 0.79,
+          "headshots": 0.428,
+          "dpr": 0.64
+        }
+      },
+      {
+        "player": "sh1ro",
+        "stats": {
+          "rating": 1.28,
+          "kpr": 0.77,
+          "headshots": 0.293,
+          "dpr": 0.50
+        }
+      },
+      {
+        "player": "nafany",
+        "stats": {
+          "rating": 0.95,
+          "kpr": 0.58,
+          "headshots": 0.485,
+          "dpr": 0.69
+        }
+      }
+    ]
   },
   {
     "logo": "https://img-cdn.hltv.org/teamlogo/SMhzsxzbkIrgqCOOKGRXlW.svg?ixlib=java-2.1.0&s=e6a9ce0345c7d703e5eaac14307f69aa",
     "name": "FaZe",
     "points": 1000,
-    "rank": 1
+    "rank": 1,
+    "roster": [
+      {
+        "player": "karrigan",
+        "stats": {
+          "rating": 0.96,
+          "kpr": 0.59,
+          "headshots": 0.474,
+          "dpr": 0.69
+        }
+      },
+      {
+        "player": "rain",
+        "stats": {
+          "rating": 1.14,
+          "kpr": 0.71,
+          "headshots": 0.501,
+          "dpr": 0.67
+        }
+      },
+      {
+        "player": "ropz",
+        "stats": {
+          "rating": 1.15,
+          "kpr": 0.73,
+          "headshots": 0.459,
+          "dpr": 0.57
+        }
+      },
+      {
+        "player": "Twisttz",
+        "stats": {
+          "rating": 1.12,
+          "kpr": 0.67,
+          "headshots": 0.594,
+          "dpr": 0.58
+        }
+      },
+      {
+        "player": "broky",
+        "stats": {
+          "rating": 1.16,
+          "kpr": 0.73,
+          "headshots": 0.315,
+          "dpr": 0.57
+        }
+      }
+    ]
   },
   {
     "logo": "https://img-cdn.hltv.org/teamlogo/9iMirAi7ArBLNU8p3kqUTZ.svg?ixlib=java-2.1.0&s=4dd8635be16122656093ae9884675d0c",
     "name": "Natus Vincere",
     "points": 837,
-    "rank": 2
+    "rank": 2,
+    "roster": [
+      {
+        "player": "s1mple",
+        "stats": {
+          "rating": 1.35,
+          "kpr": 0.88,
+          "headshots": 0.369,
+          "dpr": 0.59
+        }
+      },
+      {
+        "player": "electroNic",
+        "stats": {
+          "rating": 1.11,
+          "kpr": 0.70,
+          "headshots": 0.447,
+          "dpr": 0.65
+        }
+      },
+      {
+        "player": "Boombl4",
+        "stats": {
+          "rating": 0.99,
+          "kpr": 0.59,
+          "headshots": 0.39,
+          "dpr": 0.66
+        }
+      },
+      {
+        "player": "b1t",
+        "stats": {
+          "rating": 1.18,
+          "kpr": 0.73,
+          "headshots": 0.629,
+          "dpr": 0.60
+        }
+      },
+      {
+        "player": "Perfecto",
+        "stats": {
+          "rating": 1.07,
+          "kpr": 0.64,
+          "headshots": 0.476,
+          "dpr": 0.56
+        }
+      }
+    ]
   },
   {
     "logo": "https://img-cdn.hltv.org/teamlogo/-X8NoyWC_1gYqUHvZqcpkc.svg?ixlib=java-2.1.0&s=85bb9daa6f846fa097c5942f2565fdb8",
     "name": "ENCE",
     "points": 524,
-    "rank": 3
+    "rank": 3,
+    "roster": [
+      {
+        "player": "Snappi",
+        "stats": {
+          "rating": 0.94,
+          "kpr": 0.56,
+          "headshots": 0.48,
+          "dpr": 0.61
+        }
+      },
+      {
+        "player": "Maden",
+        "stats": {
+          "rating": 1.11,
+          "kpr": 0.69,
+          "headshots": 0.303,
+          "dpr": 0.59
+        }
+      },
+      {
+        "player": "dycha",
+        "stats": {
+          "rating": 1.22,
+          "kpr": 0.79,
+          "headshots": 0.428,
+          "dpr": 0.64
+        }
+      },
+      {
+        "player": "hades",
+        "stats": {
+          "rating": 1.12,
+          "kpr": 0.67,
+          "headshots": 0.354,
+          "dpr": 0.60
+        }
+      },
+      {
+        "player": "Spinx",
+        "stats": {
+          "rating": 1.1,
+          "kpr": 0.68,
+          "headshots": 0.419,
+          "dpr": 0.63
+        }
+      }
+    ]
   },
   {
     "logo": "https://img-cdn.hltv.org/teamlogo/zFLwAELOD15BjJSDMMNBWQ.png?ixlib=java-2.1.0&w=100&s=88aeba1564bc27de69fb2302e47e1a7c",
     "name": "G2",
     "points": 336,
-    "rank": 7
+    "rank": 7,
+    "roster": [
+      {
+        "player": "JACKZ",
+        "stats": {
+          "rating": 0.94,
+          "kpr": 0.56,
+          "headshots": 0.48,
+          "dpr": 0.61
+        }
+      },
+      {
+        "player": "NiKo",
+        "stats": {
+          "rating": 1.11,
+          "kpr": 0.69,
+          "headshots": 0.303,
+          "dpr": 0.59
+        }
+      },
+      {
+        "player": "huNter-",
+        "stats": {
+          "rating": 1.22,
+          "kpr": 0.79,
+          "headshots": 0.428,
+          "dpr": 0.64
+        }
+      },
+      {
+        "player": "Aleksib",
+        "stats": {
+          "rating": 1.12,
+          "kpr": 0.67,
+          "headshots": 0.354,
+          "dpr": 0.60
+        }
+      },
+      {
+        "player": "m0NESY",
+        "stats": {
+          "rating": 1.1,
+          "kpr": 0.68,
+          "headshots": 0.419,
+          "dpr": 0.63
+        }
+      }
+    ]
   },
   {
     "logo": "https://img-cdn.hltv.org/teamlogo/mvNQc4csFGtxXk5guAh8m1.svg?ixlib=java-2.1.0&s=11e5056829ad5d6c06c5961bbe76d20c",
     "name": "FURIA",
     "points": 328,
-    "rank": 8
+    "rank": 8,
+    "roster": [
+      {
+        "player": "aRT",
+        "stats": {
+          "rating": 0.94,
+          "kpr": 0.56,
+          "headshots": 0.48,
+          "dpr": 0.61
+        }
+      },
+      {
+        "player": "yuurih",
+        "stats": {
+          "rating": 1.11,
+          "kpr": 0.69,
+          "headshots": 0.303,
+          "dpr": 0.59
+        }
+      },
+      {
+        "player": "KSCERATO",
+        "stats": {
+          "rating": 1.22,
+          "kpr": 0.79,
+          "headshots": 0.428,
+          "dpr": 0.64
+        }
+      },
+      {
+        "player": "saffee",
+        "stats": {
+          "rating": 1.12,
+          "kpr": 0.67,
+          "headshots": 0.354,
+          "dpr": 0.60
+        }
+      },
+      {
+        "player": "drop",
+        "stats": {
+          "rating": 1.1,
+          "kpr": 0.68,
+          "headshots": 0.419,
+          "dpr": 0.63
+        }
+      }
+    ]
   },
   {
     "logo": "https://img-cdn.hltv.org/teamlogo/ywdn4tmAvXfllLeV2SkkvF.png?ixlib=java-2.1.0&w=50&s=9c70c7fbb048348f70f686acd2369c58",
     "name": "Spirit",
     "points": 347,
-    "rank": 6
+    "rank": 6,
+    "roster": [
+      {
+        "player": "chopper",
+        "stats": {
+          "rating": 0.94,
+          "kpr": 0.56,
+          "headshots": 0.48,
+          "dpr": 0.61
+        }
+      },
+      {
+        "player": "degster",
+        "stats": {
+          "rating": 1.11,
+          "kpr": 0.69,
+          "headshots": 0.303,
+          "dpr": 0.59
+        }
+      },
+      {
+        "player": "magixx",
+        "stats": {
+          "rating": 1.22,
+          "kpr": 0.79,
+          "headshots": 0.428,
+          "dpr": 0.64
+        }
+      },
+      {
+        "player": "s1ren",
+        "stats": {
+          "rating": 1.12,
+          "kpr": 0.67,
+          "headshots": 0.354,
+          "dpr": 0.60
+        }
+      },
+      {
+        "player": "Patsi",
+        "stats": {
+          "rating": 1.1,
+          "kpr": 0.68,
+          "headshots": 0.419,
+          "dpr": 0.63
+        }
+      }
+    ]
   },
   {
     "logo": "https://img-cdn.hltv.org/teamlogo/ffSPDr5mbWSKI4yruyfVtx.svg?ixlib=java-2.1.0&s=2b0a91cf4a31e6eec7a5928b10465f32",
     "name": "Heroic",
     "points": 396,
-    "rank": 4
+    "rank": 4,
+    "roster": [
+      {
+        "player": "cadiaN",
+        "stats": {
+          "rating": 0.94,
+          "kpr": 0.56,
+          "headshots": 0.48,
+          "dpr": 0.61
+        }
+      },
+      {
+        "player": "refrezh",
+        "stats": {
+          "rating": 1.11,
+          "kpr": 0.69,
+          "headshots": 0.303,
+          "dpr": 0.59
+        }
+      },
+      {
+        "player": "stavn",
+        "stats": {
+          "rating": 1.22,
+          "kpr": 0.79,
+          "headshots": 0.428,
+          "dpr": 0.64
+        }
+      },
+      {
+        "player": "TeSeS",
+        "stats": {
+          "rating": 1.12,
+          "kpr": 0.67,
+          "headshots": 0.354,
+          "dpr": 0.60
+        }
+      },
+      {
+        "player": "sjuush",
+        "stats": {
+          "rating": 1.1,
+          "kpr": 0.68,
+          "headshots": 0.419,
+          "dpr": 0.63
+        }
+      }
+    ]
   },
   {
     "logo": "https://img-cdn.hltv.org/teamlogo/mbY2YeFKUAbL9NyO6ab7-c.png?ixlib=java-2.1.0&w=100&s=95466e17ba2af2a72ec28bd1af66ceee",
     "name": "NIP",
     "points": 374,
-    "rank": 5
+    "rank": 5,
+    "roster": [
+      {
+        "player": "es3tag",
+        "stats": {
+          "rating": 0.97,
+          "kpr": 0.60,
+          "headshots": 0.316,
+          "dpr": 0.62
+        }
+      },
+      {
+        "player": "REZ",
+        "stats": {
+          "rating": 1.19,
+          "kpr": 0.74,
+          "headshots": 0.504,
+          "dpr": 0.64
+        }
+      },
+      {
+        "player": "hampus",
+        "stats": {
+          "rating": 1.02,
+          "kpr": 0.55,
+          "headshots": 0.402,
+          "dpr": 0.68
+        }
+      },
+      {
+        "player": "Brollan",
+        "stats": {
+          "rating": 1.15,
+          "kpr": 0.73,
+          "headshots": 0.527,
+          "dpr": 0.67
+        }
+      },
+      {
+        "player": "Plopski",
+        "stats": {
+          "rating": 1.03,
+          "kpr": 0.65,
+          "headshots": 0.413,
+          "dpr": 0.62
+        }
+      }
+    ]
   }
 ]
--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 import { data } from './data.js';
 
+function addAtribute(element, attribute, value) {
+  
+  // let elementCopy =  {  ...element}
+  element[attribute] = value
+  return element
+}
+
 export function changeName() {
   let container = document.querySelector('.container');
 
   data.sort((a, b) => b.points - a.points);
 
-  data.map( (teamData) => {
+  data.forEach( (teamData) => {
 
     let logo = document.createElement("img");
     logo.src = teamData.logo;
+    addAtribute(logo, "src", teamData.logo)
 
     let logoContainer = document.createElement("div");
     logoContainer.classList.add("imgContainer");
@@ -24,14 +32,64 @@ export function changeName() {
     let points = document.createElement("p");
     points.innerText = `Points: ${teamData.points}`;
     
-    let card = document.createElement("div");
-    card.classList.add("card");
+    let cardFace1 = document.createElement("div");
+    // cardFace1.classList.add("card");
+    cardFace1.classList.add("cardFront");
 
-    card.appendChild(rank);
-    card.appendChild(logoContainer);
-    card.appendChild(name);
-    card.appendChild(points);
-    container.appendChild(card);
+    cardFace1.appendChild(rank);
+    cardFace1.appendChild(logoContainer);
+    cardFace1.appendChild(name);
+    cardFace1.appendChild(points);
+  
+    let rosterTable = document.createElement("table")
+    let tableHead = document.createElement("thead")
+    let tableHeadRow = document.createElement("tr")
+    let tableBody = document.createElement("tbody")
+    
+    for (var item of ["Player","Rating"]) {
+      var th = document.createElement("th")
+      var p = document.createElement("p")
+      p.innerText = item
+      th.appendChild(p)
+      tableHeadRow.appendChild(th)
+    }
+
+    
+    tableHead.appendChild(tableHeadRow)
+    rosterTable.appendChild(tableHead)
+    rosterTable.classList.add(".tablee")
+
+    teamData.roster.forEach( roster => {
+      let player = document.createElement("tr")
+      
+      let tdName = document.createElement("td")
+      let name = document.createElement("p")
+      name.innerText = roster.player
+      tdName.appendChild(name)
+      player.appendChild(tdName)
+
+      let tdRating = document.createElement("td")
+      let rating = document.createElement("p")
+      rating.innerText = roster.stats.rating
+      tdRating.appendChild(rating)
+      player.appendChild(tdRating)
+
+      tableBody.appendChild(player)
+      rosterTable.appendChild(tableBody)
+    })
+
+    let cardFace2 = document.createElement("div")
+    cardFace2.appendChild(rosterTable)
+    // cardFace2.classList.add("card");
+    cardFace2.classList.add("cardBack");
+
+    let card = document.createElement("div")
+    card.classList.add("card")
+
+    card.appendChild(cardFace1);
+    card.appendChild(cardFace2);
+
+    container.appendChild(card)
   }); 
 }
 
